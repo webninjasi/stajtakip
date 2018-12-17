577936dcc2664bea1665c61405b488d52788298f
webninjasi <webninjasi.0@gmail.com>
2018-12-17 23:51:36 +0300

    404 sayfasını ekle

--- routes/raporlar.go
+++ routes/raporlar.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+var tpl_404 = templates.Load("templates/404.html")
 var tpl_raporlar = templates.Load("templates/raporlar.html")
 
 type Raporlar struct {
@@ -16,6 +17,19 @@ type Raporlar struct {
 
 // Verilen parametrelere göre veritabanına bir öğrenci eklemeye çalışır
 func (sh Raporlar) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		
+		err := tpl_404.ExecuteTemplate(w, "main", templates.Main{"StajTakip - Sayfa Bulunamadı"})
+		if err != nil {
+			http.Error(w, "Sayfa yüklenemedi!", http.StatusInternalServerError)
+			logrus.WithFields(logrus.Fields{
+				"err": err,
+			}).Warn("Şablon çalıştırılamadı!")
+		}
+		return
+	}
+
 	if r.Method != http.MethodGet {
 		http.Error(w, "Get metodu kullanılmalı!", http.StatusNotFound)
 		return
